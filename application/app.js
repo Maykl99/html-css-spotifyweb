6563025cf5b947c683807545e528ddd936a8289f
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-03 10:47:35 +0200

    html/css

--- application/app.js
+++ application/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $("#schermata_caricamento").fadeOut(7500);
 
+/*     $(".card").click(function(){
+        $('#song-02').css("display", "inline-block");
+    }) */
 });
+
+/* 
+$('#song-02').css("display", "inline-block");
+$('#song-03').css("display", "inline-block");
+$('#song-04').css("display", "inline-block");
+$('#song-05').css("display", "inline-block");
+$('#song-06').css("display", "inline-block"); */
