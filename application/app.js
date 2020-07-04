11445605092a79977a62c1c81faed191edac2546
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-04 13:17:51 +0200

    html/css

--- application/app.js
+++ application/app.js
@@ -1,14 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){ // tempo di esecuzione schermata di caricamento iniziale
     $("#schermata_caricamento").fadeOut(7500);
-
-/*     $(".card").click(function(){
-        $('#song-02').css("display", "inline-block");
-    }) */
 });
 
-/* 
-$('#song-02').css("display", "inline-block");
-$('#song-03').css("display", "inline-block");
-$('#song-04').css("display", "inline-block");
-$('#song-05').css("display", "inline-block");
-$('#song-06').css("display", "inline-block"); */
